b48094dd25b9f5084beb44a684408154eafd2ec1
liangmiaoling <yushen686868@163.com>
2024-12-04 14:51:06 +0800

    feat: 修改npm包和页面结构

---
+++ src/pages/login/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="login-content">
+    <wechat-outlined />
+    <span class="title">微信一键登录</span>
+  </view>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/common.scss";
+.login-content {
+  .title {
+    font-size: 48rpx;
+    color: $primary-color;
+  }
+}
+</style>
